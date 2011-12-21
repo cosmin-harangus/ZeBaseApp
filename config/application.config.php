44e83679a96e43ae7eef24ce65e5a125042ca858
Cosmin <cosmin@around25.com>
2011-12-21 13:51:18 +0200

    Migrated to Zend Framework 2 beta2.

--- config/application.config.php
+++ config/application.config.php
@@ -1,14 +1,13 @@
 <?php
 return array(
     'modules' => array(
-        'Application',
+        'Core',
     ),
     'module_listener_options' => array( 
         'config_cache_enabled' => false,
         'cache_dir'            => 'data/cache',
         'module_paths' => array(
             './module',
-            './vendor',
         ),
     ),
 );
